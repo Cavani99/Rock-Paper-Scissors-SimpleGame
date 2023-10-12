494d286c76b73a7043509dcc7d30910c9c1c294c
Kristian <kristiian.donchev@abv.bg>
2023-10-12 20:50:24 +0300

    Upgraded the code to be more playable!

--- Main.java
+++ Main.java
@@ -8,29 +8,45 @@ public class Main {
     private static final String SCISSORS="Scissors";
     private static String playerMove;
 
+    private static int GamesPlayed=0;
+    private static int Wins=0;
+    private static int Draws=0;
+    private static int Losses=0;
+
+
     static boolean PlayerChoice(){
         Scanner scanner=new Scanner(System.in);
 
-        System.out.print("Choose [R]ock,[P]aper or [S]cissors: ");
-        playerMove =scanner.nextLine();
+        while(true){
+            System.out.print("Choose [R]ock,[P]aper or [S]cissors! ");
+            System.out.print("\nWrite [Q]uit or [E]nd to finish the game!");
+            playerMove =scanner.nextLine();
 
-        if(playerMove.equalsIgnoreCase("r")
-                || playerMove.equalsIgnoreCase("Rock")) {
-            playerMove = ROCK;
-            return true;
-        }
-        else if(playerMove.equalsIgnoreCase("p")
-                || playerMove.equalsIgnoreCase("Paper")) {
-            playerMove = PAPER;
-            return true;
-        }
-        else if(playerMove.equalsIgnoreCase("s")
-                || playerMove.equalsIgnoreCase("Scissors")) {
-            playerMove = SCISSORS;
-            return true;
-        }else {
-            System.out.println("Invalid Input.Try Again...");
-            return false;
+            if(playerMove.equalsIgnoreCase("r")
+                    || playerMove.equalsIgnoreCase("Rock")) {
+                playerMove = ROCK;
+                return true;
+            }
+            else if(playerMove.equalsIgnoreCase("p")
+                    || playerMove.equalsIgnoreCase("Paper")) {
+                playerMove = PAPER;
+                return true;
+            }
+            else if(playerMove.equalsIgnoreCase("s")
+                    || playerMove.equalsIgnoreCase("Scissors")) {
+                playerMove = SCISSORS;
+                return true;
+            }else if(playerMove.equalsIgnoreCase("quit") || playerMove.equalsIgnoreCase("q")
+                    || playerMove.equalsIgnoreCase("end") || playerMove.equalsIgnoreCase("e")){
+                if(GamesPlayed==0)
+                    System.out.println("Why did you start the game ,if you are not going to play :( :( :(");
+                else
+                 System.out.printf("\nFor %d Games played you WON: %d times, DREW %d times and had %d LOSSES",
+                        GamesPlayed,Wins,Draws,Losses);
+                return false;
+            }else{
+                System.out.println("Wrong input!\n");
+            }
         }
 
     }
@@ -38,17 +54,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int stillPlay=1;
-        Scanner scanner=new Scanner(System.in);
+        String stillPlay= String.valueOf(PlayerChoice());
 
-        while (stillPlay==1) {
-            boolean rightInput = false;
+        while (stillPlay.equals("true")) {
             Random random = new Random();
 
-            while (!rightInput) {
-                rightInput = PlayerChoice();
-            }
-
+            GamesPlayed++;
 
             int computerNumber = random.nextInt(3);
             String computerMove;
@@ -56,38 +67,53 @@ public class Main {
                 case 0:
                     computerMove = ROCK;
                     System.out.printf("\nIts %s vs %s\n", playerMove, computerMove);
-                    if (playerMove.equals(computerMove))
+                    if (playerMove.equals(computerMove)){
                         System.out.println("DRAW");
+                        Draws++;
+                    }
                     else if (playerMove.equals(PAPER)) {
                         System.out.println("You WIN!");
-                    } else
+                        Wins++;
+                    } else{
                         System.out.println("You LOSE!");
+                        Losses++;
+                    }
                     break;
                 case 1:
                     computerMove = PAPER;
                     System.out.printf("\nIts %s vs %s\n", playerMove, computerMove);
-                    if (playerMove.equals(computerMove))
+                    if (playerMove.equals(computerMove)){
                         System.out.println("DRAW");
+                        Draws++;
+                    }
                     else if (playerMove.equals(SCISSORS)) {
                         System.out.println("You WIN!");
-                    } else
+                        Wins++;
+                    } else{
                         System.out.println("You LOSE!");
+                        Losses++;
+                    }
                     break;
                 case 2:
                     computerMove = SCISSORS;
                     System.out.printf("\nIts %s vs %s\n", playerMove, computerMove);
-                    if (playerMove.equals(computerMove))
+                    if (playerMove.equals(computerMove)){
                         System.out.println("DRAW");
+                        Draws++;
+                    }
                     else if (playerMove.equals(ROCK)) {
                         System.out.println("You WIN!");
-                    } else
+                        Wins++;
+                    } else{
                         System.out.println("You LOSE!");
+                        Losses++;
+                    }
                     break;
             }
 
-            System.out.println("\nDo you want to play again?\n" +
-                    "  Write 1,if the answer is positive!");
-            stillPlay=Integer.parseInt(scanner.nextLine());
+            System.out.println("\n Do you want to play again?\n" +
+                    "");
+            stillPlay= String.valueOf(PlayerChoice());
         }
 
     }
